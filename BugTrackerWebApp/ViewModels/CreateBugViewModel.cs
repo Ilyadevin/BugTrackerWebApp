8e228ef5b9aff8e541411c94e7d4084c208ed6ce
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-02-25 19:51:23 +0300

    model for creation new bug

---
+++ BugTrackerWebApp/ViewModels/CreateBugViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BugTrackerWebApp.Data.Enum;
+using BugTrackerWebApp.Interfaces;
+using BugTrackerWebApp.Models;
+
+namespace BugTrackerWebApp.ViewModels
+{
+    public class CreateBugViewModel
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? ProjectId { get; set; }
+        public int? AssignedToUserId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? ResolvedDate { get; set; }
+        public BugStatus? Status { get; set; }
+        public BugPriority? Criticality { get; set; }
+        public IFormFile? ScreenShotOfError { get; set; }
+        public AppUser? AppUser { get; set; }
+    }
+}
