2652449f0a5b418bb9afe2db26d664a89e1d5ff9
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-02-25 17:42:22 +0300

    Interfaces && Repos

---
+++ BugTrackerWebApp/Interfaces/IProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using BugTrackerWebApp.Data.Enum;
+using BugTrackerWebApp.Models;
+
+namespace BugTrackerWebApp.Interfaces
+{
+    public interface IProjectRepository
+    {
+        Task<IEnumerable<Project>> GetAll();
+        Task<Project> GetByIdAsync(int id);
+        bool Add(Project project);
+        bool Update(Project project);
+        bool Delete(Project project);
+        bool Save();
+    }
+}
---
+++ BugTrackerWebApp/Repository/BugRepository.cs
@@ -0,0 +1,57 @@
+﻿using BugTrackerWebApp.Data;
+using BugTrackerWebApp.Data.Enum;
+using BugTrackerWebApp.Interfaces;
+using BugTrackerWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BugTrackerWebApp.Repository
+{
+    public class BugRepository : IBugRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BugRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Add(Bug bug)
+        {
+            _context.Add(bug);
+            return Save();
+        }
+
+        public bool Delete(Bug bug)
+        {
+            _context.Remove(bug);
+            return Save();
+        }
+
+        public async Task<IEnumerable<Bug>> GetAll()
+        {
+            return await _context.Bugs.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Bug>> GetBugByPriority(BugPriority bugPriority)
+        {
+            return await _context.Bugs.Where(i => i.Criticality == bugPriority).ToListAsync();
+        }
+
+        public async Task<Bug> GetByIdAsync(int id)
+        {
+            return await _context.Bugs.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved>0?true: false;
+        }
+
+        public bool Update(Bug bug)
+        {
+            _context.Update(bug);
+            return Save();
+        }
+    }
+}
