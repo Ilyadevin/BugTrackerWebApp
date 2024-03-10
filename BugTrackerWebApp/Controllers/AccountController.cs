89d34e914fbaad18058d555ea327c0f0ec00731b
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-03-10 13:44:37 +0300

    AccountController

---
+++ BugTrackerWebApp/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using BugTrackerWebApp.Data;
+using BugTrackerWebApp.Models;
+using BugTrackerWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackerWebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _applicationDbContext;
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext applicationDbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid) { return View(loginViewModel); }
+            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+            if (user != null)
+            {
+                //User is found
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if (passwordCheck)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Bug");
+                    }
+                }
+                // incorrect Password
+                TempData["Error"] = "Wrong credentials, try again!";
+                return View(loginViewModel);
+            }
+            TempData["Error"] = "Wrong credentials, try again!";
+            return View(loginViewModel);
+        }
+    }
+}
