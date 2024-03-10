1541c0abb40b49dec694daf031f39710c900e871
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-03-10 10:26:12 +0300

    init the UserRolesEnum

---
+++ BugTrackerWebApp/Data/Enum/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace BugTrackerWebApp.Data.Enum
+{
+    public static class UserRoles
+    {
+        public const string Admin = "admin";
+        public const string User = "user";
+    }
+}
