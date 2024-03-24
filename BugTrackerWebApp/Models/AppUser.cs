7c9b22f8dc231040fc12fb518a681dd200be84aa
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-03-24 16:47:29 +0300

    Refactor lots of stuf connected with editing user

--- BugTrackerWebApp/Models/AppUser.cs
+++ BugTrackerWebApp/Models/AppUser.cs
@@ -3,10 +3,6 @@ namespace BugTrackerWebApp.Models
 {
     public class AppUser : Microsoft.AspNetCore.Identity.IdentityUser
     {
-        //public string? Name { get; set; }
-        //public string? Password { get; set; }
-        //public string? Role { get; set; }
-        //public string? Avatar { get; set; }
         public ICollection<Bug> Bugs { get; set; }
         public ICollection<Task_> Tasks { get; set; }
         public string? ProfileImageUrl { get; set; }
