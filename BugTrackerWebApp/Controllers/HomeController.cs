ed5af87ba7841d7d54e33dca72a7a58db1bf4ee0
Ilya Erlingas <54685842+Ilyadevin@users.noreply.github.com>
2024-03-24 18:49:35 +0300

    front+back of home finished

--- BugTrackerWebApp/Controllers/HomeController.cs
+++ BugTrackerWebApp/Controllers/HomeController.cs
@@ -1,21 +1,87 @@
+using BugTrackerWebApp.Data.Enum;
+using BugTrackerWebApp.Interfaces;
 using BugTrackerWebApp.Models;
+using BugTrackerWebApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 namespace BugTrackerWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBugRepository _bugRepository;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBugRepository bugRepository,
+            UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration config)
         {
             _logger = logger;
+            _bugRepository = bugRepository;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _config = config;
         }
 
-        public IActionResult Index()
+        public IActionResult Register()
         {
-            return View();
+            var response = new HomeUserCreateViewModel();
+            return View(response);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Bug");
+            }
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                return View(homeViewModel);
+            }
+            catch (Exception)
+            {
+                homeViewModel.Bugs= null;
+            }
+
+            return View(homeViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(HomeViewModel homeVM)
+        {
+            var createVM = homeVM.Register;
+
+            if (!ModelState.IsValid) return View(homeVM);
+
+            var user = await _userManager.FindByEmailAsync(createVM.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError("Register.Email", "This email address is already in use");
+                return View(homeVM);
+            }
+
+            var newUser = new AppUser
+            {
+                UserName = createVM.UserName,
+                Email = createVM.Email,
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, createVM.Password);
+
+            if (newUserResponse.Succeeded)
+            {
+                await _signInManager.SignInAsync(newUser, isPersistent: false);
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return RedirectToAction("Index", "Club");
         }
 
         public IActionResult Privacy()
---
+++ BugTrackerWebApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BugTrackerWebApp.Models;
+
+namespace BugTrackerWebApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Bug>? Bugs { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public HomeUserCreateViewModel Register { get; set; } = new HomeUserCreateViewModel();
+    }
+}
